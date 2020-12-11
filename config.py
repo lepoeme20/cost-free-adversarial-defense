02f8ba12d3e8eec2a7803eb982363dc3bcf2c9ac
lepoeme20 <lepoeme20@gmail.com>
2020-12-11 23:26:08 +0900

    default arguments

---
+++ config.py
@@ -0,0 +1,185 @@
+"""Set configuration for the model
+"""
+import argparse
+import multiprocessing
+import torch
+
+
+def str2float(s):
+    if '/' in s:
+        s1, s2 = s.split('/')
+        s = float(s1)/float(s2)
+    return float(s)
+
+
+def parser_setting(parser):
+    """Set arguments
+    """
+    base_args = parser.add_argument_group('base arguments')
+    base_args.add_argument(
+        '--save-path', type=str, default='./bestmodel',
+        help='save path for best model'
+        )
+    base_args.add_argument(
+        '--workers', type=int, default=multiprocessing.cpu_count()-1, metavar='N',
+        help='dataloader threads'
+        )
+    base_args.add_argument(
+        '--padding', type=int, default=4, help='base padding size'
+        )
+    base_args.add_argument(
+        '--crop-size', type=int, default=32, help='cropped image size'
+        )
+    base_args.add_argument(
+        '--dataset', type=str, default='cifar100', choices=['mnist', 'cifar10', 'cifar100'],
+        help='Dataset name'
+        )
+    base_args.add_argument(
+        "--data-root-path", type=str, default='/media/lepoeme20/Data/basics', help='data path'
+        )
+    base_args.add_argument(
+        "--n_cpu", type=int, default=multiprocessing.cpu_count(),
+        help="number of cpu threads to use during batch generation"
+        )
+    base_args.add_argument(
+        "--device-ids", type=int, nargs='*', help="device id"
+    )
+    base_args.add_argument(
+        "--network", type=str, choices=['resnet34', 'resnet110']
+    )
+
+    trn_args = parser.add_argument_group('training hyper params')
+    trn_args.add_argument(
+        '--adv-training', action='store_true', default=False,
+        help = 'if adversarial training'
+    )
+    trn_args.add_argument(
+        '--epochs', type=int, default=300, metavar='N',
+        help='number of epochs to train (default: auto)'
+        )
+    trn_args.add_argument(
+        '--finetune-epochs', type=int, default=400, metavar='N',
+        help='number of epochs to fine-tuning (default: auto)'
+        )
+    trn_args.add_argument(
+        '--batch-size', type=int, default=1024, metavar='N',
+        help='input batch size for training (default: auto)'
+        )
+    trn_args.add_argument(
+        '--test-batch-size', type=int, default=None, metavar='N',
+        help='input batch size for testing (default: auto)'
+        )
+    trn_args.add_argument(
+        '--v', type=str, default='v1', help='Experiment version'
+    )
+
+    opt_args = parser.add_argument_group('optimizer params')
+    opt_args.add_argument(
+        '--lr', type=float, default=0.1, metavar='LR', help='learning rate (default: auto)'
+        )
+    opt_args.add_argument(
+        '--b1', type=float, default=0.5, help='momentum (default: 0.9)'
+        )
+    opt_args.add_argument(
+        '--b2', type=float, default=0.99, help='momentum (default: 0.9)'
+        )
+    oth_args = parser.add_argument_group('others')
+    oth_args.add_argument(
+        "--sample-interval", type=int, default=1000, help="interval between image samples"
+        )
+    oth_args.add_argument(
+        "--dev-interval", type=int, default=500, help="interval between image samples"
+        )
+
+    attack_args = parser.add_argument_group('Attack')
+    # Deeply Supervised Discriminative Learning for Adversarial Defense (baseline)의
+    # setting을 최대한 따를 것
+    attack_args.add_argument(
+        '--attack-name', type=str, default='FGSM', choices=['Clean', 'FGSM', 'BIM', 'CW', 'PGD']
+    )
+    attack_args.add_argument(
+        '--test-model', type=str, default='pretrained_model'
+    )
+    attack_args.add_argument(
+        '--eps', type=float, default=0.02, help="For bound eta"
+    )
+    # arguments for PGD
+    attack_args.add_argument(
+        '--pgd-iters', type=int, default=10, help="# of iteration for PGD attack"
+    )
+    attack_args.add_argument(
+        '--pgd-alpha', type=float, help="Magnitude of perturbation"
+    )
+    attack_args.add_argument(
+        '--pgd-random-start', action='store_true', default=False,
+        help="If ture, initialize perturbation using eps"
+    )
+    # arguments for C&W
+    attack_args.add_argument(
+        '--cw-c', type=str2float, default=0.1, help="loss scaler"
+    )
+    attack_args.add_argument(
+        '--cw-kappa', type=float, default=0, help="minimum value on clamping"
+    )
+    attack_args.add_argument(
+        '--cw-iters', type=int, default=1000, help="# of iteration for CW grdient descent"
+    )
+    attack_args.add_argument(
+        '--cw-lr', type=float, default=0.01, help="learning rate for CW attack"
+    )
+    attack_args.add_argument(
+        '--cw-binary-search-steps', type=int, default=1, help="# of iteration for CW optimization"
+    )
+    attack_args.add_argument(
+        '--cw-targeted', action='store_true', default=False, help="d"
+    )
+    # arguments for i-FGSM
+    attack_args.add_argument(
+        '--bim-step', type=int, default=5, help="Iteration for iterative FGSM"
+    )
+
+    visualization_args = parser.add_argument_group('Visualization')
+    visualization_args.add_argument(
+        '--vis-normal', action='store_true', default=False,
+        help="Set true if you want to save normal images"
+    )
+    visualization_args.add_argument(
+        '--vis-n-rows', type=int, default=8,
+        help="# of image rows at each saved figure"
+        "default value is same as default value of nrow in torchvision.utils.save_image"
+    )
+    visualization_args.add_argument(
+        '--vis-batch-size', type=int, default=1, help=""
+    )
+    visualization_args.add_argument(
+        '--vis-set-idx', action='store_true', default=False,
+        help="Set True if you want to saving specific indices"
+    )
+    visualization_args.add_argument(
+        '--vis-indices', type=int, nargs='*',
+        help="Set indices for saving images if you set vis_set_idx True"
+    )
+
+    return parser
+
+def get_config():
+    parser = argparse.ArgumentParser(description="PyTorch Defense by distance-based model")
+    default_parser = parser_setting(parser)
+    args, _ = default_parser.parse_known_args()
+
+    args.device = torch.device(f'cuda:{args.device_ids[0]}' if torch.cuda.is_available else 'cpu')
+
+    # input channels
+    args.rgb = 1 if args.dataset == 'mnist' else 3
+
+    # number of input classes
+    # CelebA: Female/Male
+    # Cifar100: A hundred classes
+    # The rest: Ten classes
+    args.num_class = 100 if args.dataset == 'cifar100' else 10
+
+    return args
+
+
+
+

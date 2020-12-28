1a0a647c24fae984cc0d9cd43dc696a150787f17
lepoeme20 <lepoeme20@gmail.com>
2020-12-28 23:01:40 +0900

    add logger

--- trainer.py
+++ trainer.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 from utils import network_initialization, get_dataloader
 from torch.utils.tensorboard import SummaryWriter
 from utils import get_m_s, norm, get_optim
-from attack_methods import pgd
+from tqdm import tqdm
 
 class Trainer():
     def __init__(self,args):
@@ -15,14 +15,7 @@ class Trainer():
         # get mean and std for normalization
         self.m, self.s = get_m_s(args)
 
-        # check adversarial training
-        if args.adv_training:
-            root_path = os.path.join(args.save_path, 'w_adv_training')
-        else:
-            root_path = os.path.join(args.save_path, 'wo_adv_training')
-
-        save_path = os.path.join(root_path, args.dataset)
-        self.save_path = os.path.join(save_path, args.network, str(args.lr), str(args.batch_size), args.v)
+        self.save_path = os.path.join(args.save_path, args.dataset)
         os.makedirs(self.save_path, exist_ok=True)
 
         # set criterion
@@ -44,11 +37,16 @@ class Trainer():
         current_step = 0
         dev_step = 0
 
+        trn_loss_log = tqdm(total=0, position=2, bar_format='{desc}')
+        dev_loss_log = tqdm(total=0, position=4, bar_format='{desc}')
+        best_epoch_log = tqdm(total=0, position=5, bar_format='{desc}')
+        outer = tqdm(total=args.epochs, desc="Epoch", position=0, leave=False)
+
         # Train target classifier
         for epoch in range(args.epochs):
             _dev_loss = 0.0
-
-            print("")
+            train = tqdm(total=len(self.train_loader), desc="Steps", position=1, leave=False)
+            dev = tqdm(total=len(self.dev_loader), desc="Steps", position=3, leave=False)
             for step, (inputs, labels) in enumerate(self.train_loader, 0):
                 model.train()
                 current_step += 1
@@ -57,7 +55,7 @@ class Trainer():
                 inputs = norm(inputs, self.m, self.s)
 
                 # Cross entropy loss
-                logit, _, _, _ = model(inputs)
+                logit, _, features, _ = model(inputs)
                 loss = self.criterion_CE(logit, labels)
 
                 optimizer.zero_grad()
@@ -66,14 +64,21 @@ class Trainer():
                 optimizer.step()
 
                 #################### Logging ###################
-                print("[Training] Epoch {}/{} Batch {}/{} Loss: {:.4f}".format(
-                    epoch, args.epochs, step, len(self.train_loader), loss.item()
-                ), end='\r')
-                if step == 0 or step % args.sample_interval == 0:
-                    self.writer.add_scalar('train/loss', loss.item(), current_step)
-                    self.writer.close()
+                trn_loss_log.set_description_str(
+                    f"[TRN] Total Loss: {loss.item():.4f}"
+                )
+                train.update(1)
 
-            print("")
+            if epoch % 10 == 0:
+                self.writer.add_embedding(
+                    features,
+                    metadata=labels.data.cpu().numpy(),
+                    label_img=inputs,
+                    global_step=current_step,
+                    tag="[TRN]Features",
+                )
+                self.writer.close()
+
             for idx, (inputs, labels) in enumerate(self.dev_loader, 0):
                 model.eval()
                 dev_step += 1
@@ -81,7 +86,7 @@ class Trainer():
                 inputs = norm(inputs, self.m, self.s)
 
                 with torch.no_grad():
-                    logit, _, _, _ = model(inputs)
+                    logit, _, features, _ = model(inputs)
 
                     # Cross entropy loss
                     loss = self.criterion_CE(logit, labels)
@@ -90,79 +95,45 @@ class Trainer():
                     _dev_loss += loss
                     dev_loss = _dev_loss/(idx+1)
 
-                    print('[Dev] {}/{} Loss: {:.3f}'.format(
-                        idx+1, len(self.dev_loader), dev_loss), end='\r')
+                    dev_loss_log.set_description_str(
+                        f"[DEV] Loss: {dev_loss:.4f}"
+                    )
 
                     #################### Logging ###################
+                    dev.update(1)
                     if idx % args.dev_interval == 0:
-                        self.writer.add_scalar('dev/loss', loss.item(), dev_step)
+                        self.writer.add_scalar("dev/loss", loss.item(), dev_step)
                         self.writer.close()
 
-            scheduler.step(dev_loss)
-
             if dev_loss < best_loss:
+                best_epoch_log.set_description_str(
+                    f"Best Epoch: {epoch} / {args.epochs} | Best Loss: {dev_loss}"
+                )
                 best_loss = dev_loss
-                self._save_model(model, optimizer, scheduler, epoch, model_path)
+                torch.save(
+                    {
+                        "model_state_dict": model.module.state_dict(),
+                        "optimizer_state_dict": optimizer.state_dict(),
+                        "scheduler_state_dict": scheduler.state_dict(),
+                        "trained_epoch": epoch
+                    },
+                    model_path
+                )
 
+            # tensorboard logging
+            self.writer.add_scalar(
+                "train/loss", loss.item(), global_step=current_step
+            )
+            self.writer.close()
+            if epoch % 10 == 0:
+                self.writer.add_embedding(
+                    features,
+                    metadata=labels.data.cpu().numpy(),
+                    label_img=inputs,
+                    global_step=current_step,
+                    tag="[DEV]Features",
+                )
+                self.writer.close()
 
-    def _save_model(self, model, optimizer, scheduler, epoch, path):
-        print("The best model is saved")
-        torch.save({
-            'model_state_dict': model.module.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
-            'scheduler_state_dict': scheduler.state_dict(),
-            'trained_epoch': epoch,
-        }, path)
-
-    def _compute_mean(self, args, model):
-        # compute total mean vector and class mean vector
-        # construct empty tensors
-        vector_size = 512
-        class_mean = torch.zeros((args.num_class, vector_size), device=args.device)
-        class_num = torch.zeros((args.num_class, 1), device=args.device)
-
-        # feed whole w/o augmentation training set to the model for compute class mean
-        for step, (inputs, labels) in enumerate(self.wo_aug_trn_loader, 0):
-            model.eval()
-            # inputs: [batch size, channels, *(image size)]
-            # labels: [batch size]
-            inputs, labels = inputs.to(args.device), labels.to(args.device)
-            inputs = norm(inputs, self.m, self.s)
-            with torch.no_grad():
-                logit, feature_vector = model(inputs)
-                _, predicted = torch.max(logit, 1)
-
-                for i in range(inputs.size(0)):
-                    if predicted[i].eq(labels[i]):
-                        # Add output feature vector to coresponding class mean vector
-                        class_mean[labels[i]] += feature_vector[i]
-                        # Compute number of each class
-                        class_num[labels[i]] += 1
-
-        # Compute class mean vectors
-        class_mean /= class_num
-
-        # Compute total mean vector
-        total_mean = torch.mean(class_mean, 0)
-        return class_mean, total_mean
-
-    # for Adversarial Training
-#    def _get_adv_imgs(self, args, model, imgs, labels):
-#        # get pgd attack module
-#        attack_module = globals()['pgd']
-#        attack_func = getattr(attack_module, "PGD")
-#        attacker = attack_func(model, args)
-#        # get adversarial images
-#
-#        adv_imgs, adv_labels = attacker.training(imgs, labels, self.m, self.s)
-#        adv_imgs = adv_imgs.detach()
-#        imgs = torch.cat((imgs, adv_imgs), dim=0)
-#        labels = torch.cat((labels, adv_labels), dim=0)
-#
-#        # shuffle
-#        shuffle_idx = torch.randperm(labels.size(0)).to(labels.device)
-#        imgs = imgs[shuffle_idx, ...]
-#        labels = labels[shuffle_idx, ...]
-#        model.train()
-#
-#        return imgs, labels
+            scheduler.step(dev_loss)
+            outer.update(1)

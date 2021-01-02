c5b6ccb2a1badb5d3ede40deffd8a7970526ce6e
lepoeme20 <lepoeme20@gmail.com>
2021-01-02 12:32:50 +0900

    ResNet for small datasets

--- smallnet.py
+++ smallnet.py
@@ -1,56 +1,111 @@
+"""
+Created on Tue Apr  2 14:21:30 2019
+
+@author: aamir-mustafa
+"""
+import math
 import torch
 import torch.nn as nn
-from torch.nn import functional as F
+from torch.autograd import Variable
+from torch.nn import Parameter
+import torchvision.models as models
 
-import math
 
-class ConvNet(nn.Module):
-    """LeNet++ as described in the Center Loss paper."""
-    def __init__(self, num_classes=10):
-        super(ConvNet, self).__init__()
-        self.conv1_1 = nn.Conv2d(1, 32, 5, stride=1, padding=2)
-        self.prelu1_1 = nn.PReLU()
-        self.conv1_2 = nn.Conv2d(32, 32, 5, stride=1, padding=2)
-        self.prelu1_2 = nn.PReLU()
+class BasicBlock(nn.Module):
+    expansion = 1
 
-        self.conv2_1 = nn.Conv2d(32, 64, 5, stride=1, padding=2)
-        self.prelu2_1 = nn.PReLU()
-        self.conv2_2 = nn.Conv2d(64, 64, 5, stride=1, padding=2)
-        self.prelu2_2 = nn.PReLU()
-
-        self.conv3_1 = nn.Conv2d(64, 128, 5, stride=1, padding=2)
-        self.prelu3_1 = nn.PReLU()
-        self.conv3_2 = nn.Conv2d(128, 128, 5, stride=1, padding=2)
-        self.prelu3_2 = nn.PReLU()
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-
-        self.fc1 = nn.Linear(128, 512)
-        # self.prelu_fc1 = nn.PReLU()
-        self.fc2 = nn.Linear(512, 64)
-        self.fc3 = nn.Linear(64, num_classes)
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(
+            inplanes, planes, kernel_size=3, stride=stride, padding=1, bias=False
+        )
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(
+            planes, planes, kernel_size=3, stride=1, padding=1, bias=False
+        )
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
 
     def forward(self, x):
-        x = self.prelu1_1(self.conv1_1(x))
-        x = self.prelu1_2(self.conv1_2(x))
-        x = F.max_pool2d(x, 2)
+        identity = x
 
-        x = self.prelu2_1(self.conv2_1(x))
-        x = self.prelu2_2(self.conv2_2(x))
-        x = F.max_pool2d(x, 2)
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
 
-        x = self.prelu3_1(self.conv3_1(x))
-        x = self.prelu3_2(self.conv3_2(x))
+        out = self.conv2(out)
+        out = self.bn2(out)
 
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+class ResNet(nn.Module):
+    def __init__(self, depth=8, block_name='BasicBlock', num_classes=10):
+        super(ResNet, self).__init__()
+        self.inplanes = 32
+        assert (depth - 2) % 6 == 0, 'When use basicblock, depth should be 6n+2, e.g. 20, 32, 44, 56, 110, 1202'
+        n = (depth - 2) // 6
+        block = BasicBlock
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self._make_layer(block, 32, n)
+        self.layer2 = self._make_layer(block, 64, n, stride=2)
+        self.layer3 = self._make_layer(block, 128, n, stride=2)
+        self.avgpool = nn.AvgPool2d(8)
+
+        self.fc = nn.Linear(128, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
         x = self.avgpool(x)
         feature = x.view(x.size(0), -1)
-        x = self.fc1(feature)
-        x = self.fc2(x)
-        out = self.fc3(x)
+        out = self.fc(feature)
 
-        return out, None, feature, None
+        return out, feature
 
-def smallnet():
+def small_resnet(num_classes=10, block='BasicBlock'):
     """
     Constructs a ResNet model.
     """
-    return ConvNet()
+    return ResNet(num_classes=num_classes, block_name=block)
+

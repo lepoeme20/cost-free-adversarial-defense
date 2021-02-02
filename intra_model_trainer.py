75bd523a1d71d7d48ad777d00f588674158a8054
lepoeme20 <lepoeme20@gmail.com>
2021-02-03 00:50:11 +0900

    Proposed model - intra loss

--- intra_model_trainer.py
+++ intra_model_trainer.py
@@ -2,6 +2,7 @@ import os
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.distributed as dist
 from utils import network_initialization, get_dataloader
 from torch.utils.tensorboard import SummaryWriter
 from utils import (
@@ -30,7 +31,8 @@ class Trainer:
         os.makedirs(self.save_path, exist_ok=True)
 
         pretrained_path = os.path.join(self.save_path, 'restricted_model.pt')
-        self.checkpoint = torch.load(pretrained_path)
+        map_location='cuda:{}'.format(args.local_rank)
+        self.checkpoint = torch.load(pretrained_path, map_location=map_location)
         self.center = self.checkpoint["center"]
 
         # set criterion
@@ -39,14 +41,15 @@ class Trainer:
 
         # set logger path
         log_num = 0
-        if args.adv_train:
-            while os.path.exists(f"logger/proposed/intra_loss/{args.dataset}/adv_train/v{str(log_num)}"):
-                log_num += 1
-            self.writer = SummaryWriter(f"logger/proposed/intra_loss/{args.dataset}/adv_train/v{str(log_num)}")
-        else:
-            while os.path.exists(f"logger/proposed/intra_loss/{args.dataset}/v{str(log_num)}"):
-                log_num += 1
-            self.writer = SummaryWriter(f"logger/proposed/intra_loss/{args.dataset}/v{str(log_num)}")
+        if args.local_rank == 0:
+            if args.adv_train:
+                while os.path.exists(f"logger/proposed/intra_loss/{args.dataset}/adv_train/v{str(log_num)}"):
+                    log_num += 1
+                self.writer = SummaryWriter(f"logger/proposed/intra_loss/{args.dataset}/adv_train/v{str(log_num)}")
+            else:
+                while os.path.exists(f"logger/proposed/intra_loss/{args.dataset}/v{str(log_num)}"):
+                    log_num += 1
+                self.writer = SummaryWriter(f"logger/proposed/intra_loss/{args.dataset}/v{str(log_num)}")
 
     def training(self, args):
         model = self.model
@@ -69,8 +72,8 @@ class Trainer:
             model_name = f"{model_name.split('.')[0]}_adv_train.pt"
         model_path = os.path.join(self.save_path, model_name)
 
-        self.writer.add_text(tag="argument", text_string=str(args.__dict__))
-        self.writer.close()
+        if args.local_rank == 0:
+            self.writer.add_text(tag="argument", text_string=str(args.__dict__))
         best_loss = 1000
         current_step = 0
         dev_step = 0
@@ -79,11 +82,16 @@ class Trainer:
         dev_loss_log = tqdm(total=0, position=4, bar_format='{desc}')
         best_epoch_log = tqdm(total=0, position=5, bar_format='{desc}')
         outer = tqdm(total=args.epochs, desc="Epoch", position=0, leave=False)
+
         # Train target classifier
         for epoch in range(args.epochs):
             _dev_loss = 0.0
             train = tqdm(total=len(self.train_loader), desc="Steps", position=1, leave=False)
             dev = tqdm(total=len(self.dev_loader), desc="Steps", position=3, leave=False)
+
+            # let all processes sync up before starting with a new epoch of training
+            dist.barrier()
+
             for step, (inputs, labels) in enumerate(self.train_loader):
                 model.train()
                 current_step += 1
@@ -108,20 +116,25 @@ class Trainer:
                 loss.backward()
                 optimizer.step()
                 #################### Logging ###################
-                trn_loss_log.set_description_str(
-                    f"[TRN] Total Loss: {loss.item():.4f}, CE Loss: {ce_loss.item():.4f}, Intra Loss: {intra_loss.item():.4f}"
-                )
-                train.update(1)
+                if args.local_rank == 0:
+                    trn_loss_log.set_description_str(
+                        f"[TRN] Total Loss: {loss.item():.4f}, CE Loss: {ce_loss.item():.4f}, Intra Loss: {intra_loss.item():.4f}"
+                    )
+                    train.update(1)
 
-            if epoch % 10 == 0:
-                self.writer.add_embedding(
-                    features,
-                    metadata=labels.data.cpu().numpy(),
-                    label_img=inputs,
-                    global_step=current_step,
-                    tag="[TRN]Features",
-                )
-                self.writer.close()
+                    if current_step == 1 or current_step % len(self.train_loader) == 0:
+                        self.writer.add_scalar(
+                            tag="[TRN] loss", scalar_value=loss.item(), global_step=current_step
+                        )
+
+                    if current_step == 1 or current_step % (len(self.train_loader)*1) == 0:
+                        self.writer.add_embedding(
+                            features,
+                            metadata=labels.data.cpu().numpy(),
+                            label_img=inputs,
+                            global_step=current_step,
+                            tag="[TRN] Features",
+                        )
 
             for idx, (inputs, labels) in enumerate(self.dev_loader):
                 model.eval()
@@ -145,17 +158,30 @@ class Trainer:
                     _dev_loss += loss
                     dev_loss = _dev_loss / (idx + 1)
 
-                    dev_loss_log.set_description_str(
-                        f"[DEV] Loss: {dev_loss:.4f}"
-                    )
+                    if args.local_rank == 0:
+                        dev_loss_log.set_description_str(
+                            f"[DEV] Loss: {dev_loss:.4f}"
+                        )
 
-                    #################### Logging ###################
-                    dev.update(1)
-                    if idx % args.dev_interval == 0:
-                        self.writer.add_scalar("dev/loss", dev_loss.item(), dev_step)
-                        self.writer.close()
+                        #################### Logging ###################
+                        dev.update(1)
+                        if dev_step == 1 or dev_step % len(self.dev_loader) == 0:
+                            self.writer.add_scalar(
+                                tag="[DEV] loss",
+                                scalar_value=dev_loss.item(),
+                                global_step=dev_step
+                            )
 
-            if dev_loss < best_loss:
+                        if dev_step == 1 or dev_step % (len(self.dev_loader)*10) == 0:
+                            self.writer.add_embedding(
+                                features,
+                                metadata=labels.data.cpu().numpy(),
+                                label_img=inputs,
+                                global_step=dev_step,
+                                tag="[DEV] Features",
+                            )
+
+            if args.local_rank == 0 and dev_loss < best_loss:
                 best_epoch_log.set_description_str(
                     f"Best Epoch: {epoch} / {args.epochs} | Best Loss: {dev_loss}"
                 )
@@ -170,38 +196,5 @@ class Trainer:
                     model_path
                 )
 
-            # tensorboard logging
-            self.writer.add_scalar(
-                "train/loss", loss.item(), global_step=current_step
-            )
-            self.writer.add_scalar(
-                "train/ce_loss", ce_loss.item(), global_step=current_step
-            )
-            self.writer.add_scalar(
-                "train/intra_loss", intra_loss.item(), global_step=current_step
-            )
-            self.writer.close()
-            if epoch % 10 == 0:
-                self.writer.add_embedding(
-                    features,
-                    metadata=labels.data.cpu().numpy(),
-                    label_img=inputs,
-                    global_step=current_step,
-                    tag="[DEV]Features",
-                )
-                self.writer.close()
-
             scheduler.step(dev_loss)
             outer.update(1)
-
-            # save the last epoch model
-            save_last_path = f"{model_path[:-3]}_last.pt"
-            torch.save(
-                {
-                    "model_state_dict": model.module.state_dict(),
-                    "optimizer_state_dict": optimizer.state_dict(),
-                    "scheduler_state_dict": scheduler.state_dict(),
-                    "trained_epoch": epoch
-                },
-                save_last_path
-            )

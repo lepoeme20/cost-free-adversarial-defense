113427cd560749e8eefcbf9587feeefa72484a5e
lepoeme20 <lepoeme20@gmail.com>
2021-02-15 23:31:04 +0900

    restricted model is integrated with ce model

--- main.py
+++ main.py
@@ -17,16 +17,22 @@ def main():
     if args.phase == 'ce':
         print("Standard model will be trained")
         trainer = ce_trainer(args)
+        trainer.training(args)
     elif args.phase == 'inter':
         print("Inter loss model will be trained")
         trainer = inter_trainer(args)
     elif args.phase == 'restricted':
+        if not os.path.exists(f'{args.save_path}/{args.dataset}/ce_{args.ce_epoch}_model.pt'):
+            print("Standard model for Restricted loss model will be trained")
+            trainer = ce_trainer(args)
+            trainer.training(args)
         print("Restricted loss model will be trained")
         trainer = restricted_trainer(args)
+        trainer.training(args)
     else:
         print("Intra model will be trained")
         trainer = intra_trainer(args)
-    trainer.training(args)
+        trainer.training(args)
 
 if __name__ == "__main__":
     main()

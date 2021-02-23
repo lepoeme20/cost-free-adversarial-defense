4ff69eedb3b1a705560592d3a1ddc55fc5c64ec1
lepoeme20 <lepoeme20@gmail.com>
2021-02-23 22:57:38 +0900

    small model for small dataset

--- small_net.py
+++ small_net.py
@@ -6,33 +6,27 @@ class CNN(nn.Module):
     def __init__(self):
         super(CNN,self).__init__()
         self.layer = nn.Sequential(
-            nn.Conv2d(1, 16, 3, padding=1),
-            nn.BatchNorm2d(16),
+            nn.Conv2d(1, 32, 5, 1, 2),
             nn.ReLU(),
-            nn.Conv2d(16, 32, 3,padding=1),
-            nn.BatchNorm2d(32),
+            nn.Conv2d(32, 64, 5, 1, 2),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
-            nn.Conv2d(32, 64, 3,padding=1),
-            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 128, 5, 1, 2),
             nn.ReLU(),
-            nn.MaxPool2d(2, 2)
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(128, 128, 5, 1, 2),
+            nn.ReLU()
         )
-        self.fc1 = nn.Sequential(
-            nn.Linear(64*7*7, 512),
-            nn.BatchNorm1d(512),
-            nn.ReLU(),
-        )
-        self.fc2 = nn.Sequential(
-            nn.Linear(512, 128),
-            nn.BatchNorm1d(128),
-            nn.ReLU(),
+        self.avg_pool = nn.AvgPool2d(6)
+        self.fc_layer = nn.Sequential(
             nn.Linear(128, 10),
         )
     def forward(self,x):
         out = self.layer(x)
-        out = out.view(out.size(0), -1)
-        features = self.fc1(out)
-        out = self.fc2(features)
-
+        out = self.avg_pool(out)
+        features = out.view(out.size(0), -1)
+        out = self.fc_layer(features)
         return out, features
+
+def smallnet():
+    return CNN()

c5cd3593985004177d6f39e1f4bad47e46e2c68e
lepoeme20 <lepoeme20@gmail.com>
2021-02-25 23:20:40 +0900

    use LeNet for small size dataset

--- small_net.py
+++ small_net.py
@@ -28,5 +28,36 @@ class CNN(nn.Module):
         out = self.fc_layer(features)
         return out, features
 
+
+class LeNet5(nn.Module):
+
+    def __init__(self, n_classes=10):
+        super(LeNet5, self).__init__()
+
+        self.feature_extractor = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, stride=1),
+            nn.Tanh(),
+            nn.AvgPool2d(kernel_size=2),
+            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1),
+            nn.Tanh(),
+            nn.AvgPool2d(kernel_size=2),
+            nn.Conv2d(in_channels=16, out_channels=120, kernel_size=5, stride=1),
+            nn.Tanh()
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(in_features=120, out_features=84),
+            nn.Tanh(),
+            nn.Linear(in_features=84, out_features=n_classes),
+        )
+
+
+    def forward(self, x):
+        x = self.feature_extractor(x)
+        features = torch.flatten(x, 1)
+        logits = self.classifier(features)
+        return logits, features
+
+
 def smallnet():
-    return CNN()
+    return LeNet5()

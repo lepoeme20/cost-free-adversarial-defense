efe392d538c36b1375b1a628999bd7dbca8e8b5e
lepoeme20 <lepoeme20@gmail.com>
2021-05-10 10:45:42 +0900

    best performance on CIFAR10

--- train.sh
+++ train.sh
@@ -5,13 +5,13 @@
 # === Set parameters ===
 dataset='cifar100' #cifar10 | cifar100
 adv_training='false'
-phase_tuple=('restricted'  'intra') # ce | inter | restricted | intra
+phase_tuple=('restricted' 'intra') # ce | inter | restricted | intra
 model='110' # renet | 18 | 34 | 110: str
 restrict_dist=6
 gpu_id=(0 1 2 3)
 data_path='/repo/data/'
 lr=0.1
-lr_intra=0.00005 # CIFAR10: 0.001
+lr_intra=0.001 # CIFAR10: 0.001
 batch_size=512
 
 echo "**START Training | $inter_p | $intra_p**"
@@ -30,11 +30,11 @@ do
         epochs=0
     elif [ $phase = 'restricted' ]
     then
-        ce_epoch=100
-        epochs=150
+        ce_epoch=50
+        epochs=100
     else
         ce_epoch=50
-        epochs=500
+        epochs=100
     fi
 python main.py --batch-size $batch_size --lr $lr --lr-intra $lr_intra \
     --dataset $dataset --epochs $epochs --ce-epoch $ce_epoch --data-root-path $data_path \

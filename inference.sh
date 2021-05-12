c7f390289f5939363ca785059c0e924a5d2b06da
lepoeme20 <lepoeme20@gmail.com>
2021-05-12 12:20:36 +0900

    clear print

--- inference.sh
+++ inference.sh
@@ -1,42 +1,40 @@
 #!/bin/bash
 
-# To save adversaria images
-
 # === Set parameters ===
 dataset='cifar100' #cifar10 | cifar100
 attack_methods=('Clean' 'FGSM'  'BIM'  'PGD') # 'Clean' 'FGSM' 'BIM' 'PGD'
-epsilon=0.03
+epsilon=(0.03)
 models=('intra') # ce | inter | restricted | intra
 backbone='110'
-adv_training='false'
 data_path='/repo/data'
 tst_batch_size=1024
 device_ids=(0 2 3)
 
-# for v in ${version[@]}
+for e in ${epsilon[@]}
+do
     for model in ${models[@]}
     do
         for attack_method in ${attack_methods[@]}
         do
         echo ""
-        echo "**START INFERENCE Trn mini-batch size $b Model**"
+        echo "**START INFERENCE**"
         echo "-------------------------------------------------"
         if [ "$attack_method" != "Clean" ]; then
-            echo "Performance on '$attack_method' attack with '$epsilon' epsilon"
+            echo "Performance on '$attack_method' attack with '$e' epsilon"
         else
             echo "Performance on 'clean' dataset:"
         fi
         echo "Dataset: $dataset"
-        echo "Test model: $model"
-        echo "Adversarial Training: '$adv_training'"
+        echo "Test model: $model (backbone: $backbone)"
         echo "-------------------------------------------------"
         echo ""
 
         python inference.py --attack-name $attack_method \
-            --test-model $model --dataset $dataset --eps $epsilon \
+            --test-model $model --dataset $dataset --eps $e \
             --data-root-path $data_path \
             --device-ids ${device_ids[@]} \
             --model $backbone \
             --test-batch-size $tst_batch_size # --adv-train
         done
     done
+done

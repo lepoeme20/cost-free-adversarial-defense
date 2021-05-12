19c6d0a8c3a732fd3d515f026452e1cf34d8b1a8
lepoeme20 <lepoeme20@gmail.com>
2021-05-12 14:42:42 +0900

    clear print

--- train.sh
+++ train.sh
@@ -3,24 +3,17 @@
 # To save adversaria images
 
 # === Set parameters ===
-dataset='cifar100' #cifar10 | cifar100
+dataset='cifar10' #cifar10 | cifar100
 adv_training='false'
-phase_tuple=('intra') # ce | inter | restricted | intra
+phase_tuple=('restricted'  'intra') # ce | inter | restricted | intra
 model='110' # renet | 18 | 34 | 110: str
 restrict_dist=6
 gpu_id=(0 2 3) # (0 1 2 3)
 data_path='/repo/data/'
 lr=0.1
-lr_intra=0.0001 # CIFAR10: 0.001
+lr_intra=0.001 # CIFAR10: 0.001
 batch_size=512
 
-echo "**START Training | $inter_p | $intra_p**"
-echo "-------------------------------------------------------------------------"
-echo "Dataset: $dataset"
-echo "Adversarial Training: '$adv_training'"
-echo "Train with lr $lr_pre for $epochs epochs"
-echo "-------------------------------------------------------------------------"
-echo ""
 
 for phase in ${phase_tuple[@]}
 do
@@ -33,12 +26,19 @@ do
         ce_epoch=50
         epochs=100
     else
-        ce_epoch=100
+        ce_epoch=0
         epochs=1000
     fi
+    echo "**START Training**"
+    echo "-------------------------------------------------------------------------"
+    echo "Dataset: $dataset"
+    echo "Test model: $phase (backbone: $model)"
+    echo "Adversarial Training: '$adv_training'"
+    echo "-------------------------------------------------------------------------"
+    echo ""
 python main.py --batch-size $batch_size --lr $lr --lr-intra $lr_intra \
     --dataset $dataset --epochs $epochs --ce-epoch $ce_epoch --data-root-path $data_path \
-    --device-ids ${gpu_id[@]} --phase $phase --restrict-dist $restrict_dist --model $model \
-    --resume --resume-model 'intra_663_model_110'
+    --device-ids ${gpu_id[@]} --phase $phase --restrict-dist $restrict_dist --model $model
+    # --resume --resume-model 'intra_663_model_110'
 done
 

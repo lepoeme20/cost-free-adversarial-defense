e04b260f3553b384d2c6519e98747bf5ff2666ff
lepoeme20 <lepoeme20@gmail.com>
2021-05-16 00:03:32 +0900

    add baseline 6-layer, ResNet-18

--- small_net.py
+++ small_net.py
@@ -1,5 +1,32 @@
 import torch
 import torch.nn as nn
+from torchvision.models.resnet import ResNet, BasicBlock, Bottleneck
+
+class Custom_ResNet18(ResNet):
+    def __init__(self):
+        super(Custom_ResNet18, self).__init__(
+            BasicBlock, [2, 2, 2, 2], num_classes=10
+        ) # Based on ResNet18
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=1, padding=3,bias=False)
+
+class ResNet18(nn.Module):
+    def __init__(self):
+        super(ResNet18, self).__init__()
+        backbone = Custom_ResNet18()
+        self.features = nn.Sequential(
+            *list(backbone.children())[:-2]
+        )
+        self.avg_pool = nn.AvgPool2d(2)
+        self.fc = nn.Linear(512, 10)
+
+    def forward(self, x):
+        out = self.features(x)
+        out = self.avg_pool(out)
+        features = out.view(out.size(0), -1)
+        logit = self.fc(features)
+
+        return logit, features
+
 
 class Rep(nn.Module):
     def __init__(self, n_classes=10):
@@ -24,7 +51,7 @@ class Rep(nn.Module):
             nn.Linear(128, 64),
             nn.Linear(64, n_classes)
         )
-        self.avg_pool = nn.AvgPool2d(8)
+        self.avg_pool = nn.AvgPool2d(4)
 
     def forward(self, x):
         x = self.feature_extractor(x)
@@ -34,34 +61,36 @@ class Rep(nn.Module):
 
         return out, features
 
-class LeNet5_(nn.Module):
+
+class LeNet5(nn.Module):
     def __init__(self, n_classes=10):
         super(LeNet5, self).__init__()
 
         self.feature_extractor = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5),
-            nn.ReLU(),
+            nn.Conv2d(1, 6, 3, stride=1, padding=1),
             nn.MaxPool2d(2, 2),
-            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5),
-            nn.ReLU(),
+            nn.Conv2d(6, 16, 5, stride=1, padding=0),
             nn.MaxPool2d(2, 2),
-            nn.Conv2d(16, 120, 5),
-            nn.ReLU()
         )
 
         self.fc = nn.Sequential(
-            nn.Linear(in_features=120, out_features=84),
-            nn.ReLU(),
+            nn.Linear(400, 120),
+            nn.Linear(120, 84),
             nn.Linear(in_features=84, out_features=n_classes),
         )
 
 
     def forward(self, x):
         x = self.feature_extractor(x)
-        features = torch.flatten(x, 1)
+        features = x.view(x.size(0), -1)
         logits = self.fc(features)
         return logits, features
 
 
-def smallnet():
-    return LeNet5()
+def smallnet(model_type):
+    if model_type == '18':
+        return ResNet18()
+    elif model_type == 'lenet':
+        return LeNet5()
+    else:
+        return Rep()

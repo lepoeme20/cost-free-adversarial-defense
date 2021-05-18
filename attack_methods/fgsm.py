70e7e853cc8871318532cb2c676ac5b7b1546b9e
lepoeme20 <lepoeme20@gmail.com>
2021-05-18 22:31:15 +0900

    fgsm

--- attack_methods/fgsm.py
+++ attack_methods/fgsm.py
@@ -12,20 +12,17 @@ class FGSM(Attack):
         super(FGSM, self).__init__("FGSM", target_cls)
         self.eps = args.eps
         self.criterion = nn.CrossEntropyLoss()
-        self.args = args
 
     def forward(self, imgs, labels, norm_fn, m, s):
         imgs = imgs.clone().detach()
-        labels = labels
 
         imgs.requires_grad = True
 
         outputs, _ = self.target_cls(norm_fn(imgs, m, s))
         loss = self.criterion(outputs, labels)
-        loss.backward()
 
-        grad = imgs.grad.data
-
+        grad = torch.autograd.grad(
+            loss, imgs, retain_graph=False, create_graph=False)[0]
         adv_imgs = imgs+(self.eps*grad.sign())
         adv_imgs = torch.clamp(adv_imgs, 0, 1)
 

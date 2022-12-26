29cef454f4af0a6c733b9a2f872feae870202052
lepoeme20 <lepoeme20@gmail.com>
2022-12-26 15:26:37 +0900

    Radial Phase

--- radial_phase.py
+++ radial_phase.py
@@ -2,17 +2,16 @@ import os
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.distributed as dist
 from utils import network_initialization, get_dataloader
 from utils import (
     get_m_s,
     norm,
     get_optim,
     Loss,
-    set_seed,
     get_center,
-    LargeMarginLoss,
+    set_seed
 )
+from attack_methods import pgd, fgsm
 from tqdm import tqdm
 import datetime
 import time
@@ -31,23 +30,11 @@ class Trainer:
         self.save_path = os.path.join(args.save_path, args.dataset)
         os.makedirs(self.save_path, exist_ok=True)
 
-        if args.resume:
-            pretrained_path = os.path.join(
-                self.save_path, f'{args.resume_model}.pt'
-            )
-        else:
-            pretrained_path = os.path.join(
-                self.save_path,
-                f'restricted_model_{args.model}.pt'
-            )
+        pretrained_path = os.path.join(
+            self.save_path, f'ce_{args.ce_epoch}_model_{args.model}.pt'
+        )
         self.checkpoint = torch.load(pretrained_path)
         self.model.module.load_state_dict(self.checkpoint["model_state_dict"])
-        try:
-            for param in self.model.module.fc.parameters():
-                param.requires_grad = False
-        except:
-            for param in self.model.module.classifier.parameters():
-                param.requires_grad = False
         self.center = get_center(
             self.model, self.train_loader, args.num_class, args.device, self.m, self.s
         )
@@ -62,39 +49,29 @@ class Trainer:
         )
 
     def training(self, args):
-        # set optimizer & scheduler
+        # load the model weights
         optimizer, scheduler = get_optim(
-            self.model, args.lr_intra, intra=True
+            self.model, args.lr
         )
+        optimizer.load_state_dict(self.checkpoint["optimizer_state_dict"])
 
-        # base model
-        model_name = f"intra_model_{args.model}.pt"
+        model_name = f"restricted_model_scale_{args.model}.pt"
         model_path = os.path.join(self.save_path, model_name)
-        # model_path = os.path.join(self.save_path, 'ablation', model_name)
 
         best_loss = 1000
         current_step = 0
         dev_step = 0
 
-        if args.resume:
-            optimizer.load_state_dict(self.checkpoint["optimizer_state_dict"])
-            scheduler.load_state_dict(self.checkpoint["scheduler_state_dict"])
-            trained_epoch = self.checkpoint["trained_epoch"] + 1
-            best_loss = self.checkpoint["best_loss"]
-        else:
-            trained_epoch = 0
-
         trn_loss_log = tqdm(total=0, position=2, bar_format='{desc}')
         dev_loss_log = tqdm(total=0, position=4, bar_format='{desc}')
         best_epoch_log = tqdm(total=0, position=5, bar_format='{desc}')
-        outer = tqdm(total=args.epochs-trained_epoch, desc="Epoch", position=0, leave=False)
+        outer = tqdm(total=args.epochs, desc="Epoch", position=0, leave=False)
 
-        os.makedirs('time_log', exist_ok=True)
         f = open(f"time_log/{args.dataset}_{args.phase}.txt", 'w')
         start_total = time.time()
 
-        # train target classifier
-        for epoch in range(trained_epoch, args.epochs):
+        # Train target classifier
+        for epoch in range(args.epochs):
             start_epoch = time.time()
             _dev_loss = 0.0
             train = tqdm(total=len(self.train_loader), desc="Steps", position=1, leave=False)
@@ -110,22 +87,20 @@ class Trainer:
                 inputs = norm(inputs, self.m, self.s)
 
                 logit, features = self.model(inputs)
-                _, predict = torch.max(logit, 1)
-                correct_idx = predict.eq(labels)
                 ce_loss = self.criterion_CE(logit, labels)
-                intra_loss = self.criterion(features, labels, correct_idx)
-
-                loss = 0.00*ce_loss + 10.0*intra_loss
+                restricted_loss = self.criterion(features, labels)
+                loss = ce_loss + restricted_loss
 
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
                 #################### Logging ###################
                 trn_loss_log.set_description_str(
-                    f"[TRN] Total Loss: {loss.item():.4f}, CE Loss: {ce_loss.item():.4f}, Intra Loss: {intra_loss.item():.4f}"
+                    f"[TRN] Total Loss: {loss.item():.4f}, CE Loss: {ce_loss.item():.4f}, Restricted Loss: {restricted_loss.item():.4f}"
                 )
                 train.update(1)
 
+            # write epoch time
             epoch_time = round(time.time() - start_epoch)
             epoch_time = str(datetime.timedelta(seconds=epoch_time))
             f.write(f"Epoch {epoch+1}: "+str(epoch_time)+'\n')
@@ -137,28 +112,28 @@ class Trainer:
                 if inputs.size(1) == 1:
                     inputs = inputs.repeat(1, 3, 1, 1)
                 inputs, labels = inputs.to(args.device), labels.to(args.device)
+                if args.adv_train:
+                    adv_imgs, adv_labels = attacker.__call__(inputs, labels, norm, self.m, self.s)
+                    inputs = torch.cat((inputs, adv_imgs), 0)
+                    labels = torch.cat((labels, adv_labels))
                 inputs = norm(inputs, self.m, self.s)
 
                 with torch.no_grad():
                     logit, features = self.model(inputs)
-                    _, predict = torch.max(logit, 1)
-                    correct_idx = predict.eq(labels)
-
                     ce_loss = self.criterion_CE(logit, labels)
-                    intra_loss = self.criterion(features, labels, correct_idx)
-                    loss = intra_loss
+                    restricted_loss = self.criterion(features, labels)
+                    loss = ce_loss + restricted_loss
 
                     # Loss
                     _dev_loss += loss
                     dev_loss = _dev_loss / (idx + 1)
 
                     dev_loss_log.set_description_str(
-                        f"[DEV] CE Loss: {ce_loss.item():.4f} Intra Loss: {intra_loss.item():.4f}"
+                        f"[DEV] Total Loss: {dev_loss.item():.4f}, CE Loss: {ce_loss.item():.4f}, Restricted Loss: {restricted_loss.item():.4f}"
                     )
-
                     dev.update(1)
 
-            if dev_loss < best_loss:
+            if dev_loss > best_loss:
                 best_epoch_log.set_description_str(
                     f"Best Epoch: {epoch} / {args.epochs} | Best Loss: {dev_loss}"
                 )
@@ -169,18 +144,16 @@ class Trainer:
                         "optimizer_state_dict": optimizer.state_dict(),
                         "scheduler_state_dict": scheduler.state_dict(),
                         "trained_epoch": epoch,
-                        "best_loss": dev_loss,
                         "center": self.center
                     },
-                    # model_path[:-12] + str(epoch) + '_' + model_path[-12:]
-                    # f"{model_path.split('_')[0]}_{str(epoch)}_{'_'.join(model_path.split('_')[1:])}"
-                    # model_path[:-8] + str(epoch) + '_' + model_path[-8:]
                     model_path
                 )
+
 
             scheduler.step(dev_loss)
             outer.update(1)
 
+        # write total time
         total_time = round(time.time() - start_total)
         total_time = str(datetime.timedelta(seconds=total_time))
         f.write(f"Total: "+str(total_time)+'\n')
